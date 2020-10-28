5e2c6944cf0940ea1c1d00c9d6c911e39c848057
krishna <krishna.nature91@gmail.com>
2020-10-28 16:44:13 +0000

    Added main go file

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import(
-	"fmt"
+import (
+	"log"
+	"os"
+	"todoAPI/config"
+	"todoAPI/migration"
+	"todoAPI/route"
+
+	"github.com/gin-gonic/gin"
 )
 
+// gets the Database instance and creates the
+// Migration on it (if not exist)
+func init() {
+	db := config.Init()
+	migration.Migrate(db)
+}
+
 func main() {
-	fmt.Println("HELLO WORLD!")
+	gin.SetMode(gin.ReleaseMode)
+
+	// setup function for deploy the routes
+	router := route.SetupRoutes()
+
+	// finally creates a server with the indicated port.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
 }
